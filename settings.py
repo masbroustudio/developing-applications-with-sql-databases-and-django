b9bf9488df658db380aabffe43bad74103f27a7c
Yudha E <masbroumail@gmail.com>
2023-07-12 02:43:26 -0400

    Create a Django Project in Cloud IDE Week 2

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MTg3OTMteXVkaGFl',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
